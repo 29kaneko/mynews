767c7fc0f1b11dfae5b935980f5cd0b2b7487c29
kaneko <kaneko.yosuke0222@icloud.com>
2020-08-31 10:06:04 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,11 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+Route::group(['prefix' => 'XXX'], function() {
+    Route::get('XXX', 'AAAController@bbb');
+});
+
