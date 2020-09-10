7287e9e51e2c4f04a8e51913ce44e212a4bbbec6
kaneko <kaneko.yosuke0222@icloud.com>
2020-09-10 06:52:56 +0000

    first commit

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,4 +10,17 @@
     <body>
         <h1>Myprofile作成画面</h1>
     </body>
+
+@extends('layouts.admin')
+@section('title', 'Profileの新規作成')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Profile新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
 </html>
